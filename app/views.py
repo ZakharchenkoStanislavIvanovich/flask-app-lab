5ae28dc303ded0f172a23dfe39c8f27a7ce17a76
ZakharchenkoStanislavIvanovich <stanislav.zakharchenko.22@pnu.edu.ua>
2024-12-07 20:32:09 +0200

    app as module

--- app/views.py
+++ app/views.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, redirect, url_for, render_template
-
-app = Flask(__name__)
-app.config.from_pyfile("config.py")
+from flask import (abort, request, redirect, url_for, render_template)
+from . import app
 
 @app.route('/')
 def main():
@@ -49,6 +47,3 @@ def detail_post(id):
         abort(404)
     post = posts[id-1]
     return render_template("detail_post.html", post=post)
-
-if __name__ == '__main__':
-    app.run(debug=True)

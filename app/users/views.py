7a657d63ecaf9f0a9a9d1e3af3e29d36428fd5e7
ZakharchenkoStanislavIvanovich <stanislav.zakharchenko.22@pnu.edu.ua>
2024-12-11 19:02:39 +0200

    add cookie

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
 from . import bp
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, make_response
+from datetime import timedelta
 
 @bp.route('/hi/<string:name>') #/hi/stas?age=30
 def greetings(name):
@@ -14,3 +15,22 @@ def admin():
     to_url = url_for("user_name.greetings", name="administrator", age=45, _external=True) #http://localhost:8080/hi/administrator"
     print(to_url)
     return redirect(to_url)
+
+@bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кукі встановлений')
+    #response.set_cookie('username', 'student', expires=datetime.now()+timedelta(seconds=10))
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', max_age=timedelta(seconds=60))
+    return response
+
+@bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+@bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response ('Кукі видалений')
+    response.set_cookie('username', '', expires=0) #response.set_cookie('username', '', max_age=0)
+    return response
